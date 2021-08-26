a70867021dc5458071f783a7ab591ea5b0d86993
letchugo <specialchoo@naver.com>
2021-08-26 23:07:17 +0900

    Category 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 import os
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -15,6 +24,8 @@ class Post(models.Model):
     update_at = models.DateTimeField(auto_now=True) #자동으로 작성시각과 수정시각 저장하는것
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
